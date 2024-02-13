176a7ecb21f8e8d4c0788853f317eb566c79dab6
Paresh-Kalsotra <pareshkalsotra@gmail.com>
2024-02-13 16:02:11 +0530

    function concepts

---
+++ daily_assignments/13-02-2024/script.js
@@ -0,0 +1,62 @@
+//-------- object creation
+const student = {
+  Name: "virat",
+  age: 20,
+  grade: "a",
+  subjects: ["mathematics", "statistics", "programming", "economics"],
+  marks: [80, 60, 70, 90],
+};
+
+//-----------------------------
+//func to display information
+function displayInfo(message) {
+  console.log(message + this.Name);
+  console.log(this.Name);
+  console.log(this.age);
+  console.log(this.grade);
+}
+
+// binding object and func
+const boundDisplayInfo = displayInfo.bind(student);
+
+boundDisplayInfo("hello !!"); //calling bounded func
+
+//func call using call and apply
+displayInfo.call(student, "Details of student, ");
+boundDisplayInfo.apply(null, ["Details of "]);
+
+//----------------------------
+subArray = [" english", " history", " biology"];
+
+// function callback
+function processSubjects(arr, func) {
+  let newarray = arr.concat(func(subArray));
+  console.log(newarray);
+}
+const ret = (Array) => {
+  return Array;
+};
+
+processSubjects(student.subjects, ret);
+
+//------------------------
+//map and filter
+
+function doubleGrade(array) {
+  return array.map((x) => x * 2); //map method
+}
+
+function passingSubject(arr) {
+  let passSub = [];
+  arr.filter((num, key) => {
+    if (num >= 70) {
+      passSub.push(student.subjects[key]);
+    }
+  });
+  return passSub;
+}
+
+console.log(`Original Grades: ${student.marks}`);
+console.log(`modified Grades: ${doubleGrade(student.marks)}`); //calling func with map method
+
+console.log(`passed subject: ${passingSubject(student.marks)}`); //calling func with filter method
